965ed14e68107255d6d83100d2fd0c82b818e763
Mianxiong <757134184@qq.com>
2023-02-15 11:57:52 +0800

    deploy

---
+++ precache-manifest.5ea875115ac5951ad917a97343ea93ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52270170da3b31c75ce03fffe13b9e5d",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "9584f812771f68d36e4e",
+    "url": "/morney-website/static/css/main.a45f5f87.chunk.css"
+  },
+  {
+    "revision": "aaa51382be14d1ad343d",
+    "url": "/morney-website/static/js/2.7d0180a7.chunk.js"
+  },
+  {
+    "revision": "f5f9f97d0ed82059dcb9c4d6ad6d7f46",
+    "url": "/morney-website/static/js/2.7d0180a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9584f812771f68d36e4e",
+    "url": "/morney-website/static/js/main.8c42ea19.chunk.js"
+  },
+  {
+    "revision": "3b29e10f15b2de5207e3",
+    "url": "/morney-website/static/js/runtime-main.86260666.js"
+  }
+]);
